c518a97074a7c0487d3f9c3f08d2b0c3fcd50e7f
forrana <forrana@gmail.com>
2015-11-11 16:35:11 +0300

    release #2 add templates/emails directory with simple email template, also add mechanism to generate emails from templates

--- orders/models.py
+++ orders/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from prices.models import Price
 from django.core.mail import send_mail
 from django.core.mail import EmailMessage
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+from django.template import Context
 
 class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -13,9 +16,17 @@ class Order(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Send e-mail to current user
+        Send e-mail to current user and then save order
         """
-        email = EmailMessage('Irden', 'Hi, '+self.client_name, to=[self.email])
-        email.send()
+        plaintext = get_template('emails/confirmation.txt')
+        htmly     = get_template('emails/confirmation.html')
+        d = Context({ 'client_name': self.client_name,  'program_name': self.price.name, 'cost': self.price.cost})
+        subject, from_email, to = 'Hello from Irden', 'irden@gmail.com', self.email
+        text_content = plaintext.render(d)
+        html_content = htmly.render(d)
+        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+
         self.email_status = 'email sended'
         super(Order, self).save(*args, **kwargs)
