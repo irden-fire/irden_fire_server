6fd879b532dcf54a9262f7feed496e51c943a95f
forrana <forrana@gmail.com>
2015-11-20 01:30:18 +0300

    release #5, add admin templates, rewrite in more human pace customer template, send both emails at the same time

--- orders/models.py
+++ orders/models.py
@@ -18,19 +18,41 @@ class Order(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Send e-mail to current user and then save order
+        Send e-mail to current user
         """
         plaintext = get_template('emails/confirmation.txt')
         htmly     = get_template('emails/confirmation.html')
         d = Context({   'client_name': self.client_name,
                         'program_name': self.price.name,
                         'cost': self.price.cost})
-        subject, from_email, to = 'Hello from Irden', 'irdenfire@gmail.com', self.email
+        subject, from_email, to = 'Your order was processed', 'irdenfire@gmail.com', self.email
         text_content = plaintext.render(d)
         html_content = htmly.render(d)
-        msg = EmailMultiAlternatives(subject, text_content, from_email, [to], ['forrana@gmail.com'])
+        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
         msg.attach_alternative(html_content, "text/html")
         msg.send()
 
+        """
+        Send e-mail to admins
+        """
+        plaintext = get_template('emails/to_admins.txt')
+        htmly     = get_template('emails/to_admins.html')
+        d = Context({   'client_name': self.client_name,
+                        'program_name': self.price.name,
+                        'cost': self.price.cost,
+                        'phone_number': self.contact_number,
+                        'program_id': self.price.pk,
+                        'desired_date': self.desired_date
+                        })
+        subject, from_email, to = 'Attention, new order was placed', 'irdenfire@gmail.com', ['forrana@gmail.com', 'forrana@yandex.ru']
+        text_content = plaintext.render(d)
+        html_content = htmly.render(d)
+        msg = EmailMultiAlternatives(subject, text_content, from_email, to)
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+
+        """
+        Set status to 'sended' and save record
+        """
         self.email_status = 'Sended'
         super(Order, self).save(*args, **kwargs)
